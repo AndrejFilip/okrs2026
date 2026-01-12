2aa50b913c818c0da347f714fc4f23af52de0c9e
Andrej Bielik <andrej.bielik@surglogs.com>
2026-01-12 11:48:18 +0100

    feat: add styling to layout

--- okrs2026/src/app/auth/login/layout.tsx
+++ okrs2026/src/app/auth/login/layout.tsx
@@ -3,5 +3,13 @@ export default function AuthLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div className="bg-white min-h-screen">{children}</div>;
+  return (
+    <div
+      {...{
+        className: "bg-white min-h-screen flex items-center justify-center ",
+      }}
+    >
+      {children}
+    </div>
+  );
 }
